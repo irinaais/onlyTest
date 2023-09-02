d0bcce0b5f648c0d40fb8231e231198b0f6ddcad
Iris <irinaais90@gmail.com>
2023-09-02 13:14:05 +0300

    feat: add open and close button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-const a = 1;
-console.log(a);
+const template = document.querySelector('#template');
 
+window.addEventListener('click', (evt) => {
+  let target = evt.target;
+
+  const smallBuilding = target.classList.contains('button_variant_small-building');
+  const smallBuildingOpened = target.classList.contains('button-opened_variant_small-building');
+
+  if (smallBuilding) {
+    console.log('smallBuilding');
+    const templateItem = template.content.cloneNode(true);
+    const div = templateItem.querySelector('div');
+    div.classList.add('button-opened_color_blue','button-opened_variant_small-building');
+    const span = templateItem.querySelector('span');
+    span.textContent = 'Здание';
+
+    target.replaceWith(templateItem);
+  }
+
+  if (smallBuildingOpened) {
+    console.log('smallBuildingOpened');
+    const button = document.createElement('button');
+    button.classList.add('button', 'button_type_open', 'button_color_blue', 'button_variant_small-building');
+    target.replaceWith(button);
+  }
+
+})
