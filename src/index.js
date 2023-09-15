4cd69e17c59a53d8b221163d6af885e8bd94c8b2
Irina Osipova <irinaais90@gmail.com>
2023-09-15 16:17:58 +0300

    refactor: addEventListener

--- src/index.js
+++ src/index.js
@@ -13,117 +13,64 @@ const apartmentsDescription = 'Апартаменты';
 
 document.addEventListener('click', (evt) => {
   if (openedButton === null) {
-    let target = evt.target;
+    const target = evt.target;
+    const button = target.closest('.button');
 
-    const smallBuilding = target.classList.contains('button_variant_small-building');
-    const stadium = target.classList.contains('button_variant_stadium');
-    const bridge = target.classList.contains('button_variant_bridge');
-    const flat = target.classList.contains('button_variant_flat');
-    const museum = target.classList.contains('button_variant_museum');
-    const showroom = target.classList.contains('button_variant_showroom');
-    const administration = target.classList.contains('button_variant_administration');
-    const office1 = target.classList.contains('button_variant_office1');
-    const office2 = target.classList.contains('button_variant_office2');
-    const apartments = target.classList.contains('button_variant_apartments');
-
-    const plusImg = target.classList.contains('img_type_plus');
-
-    if (smallBuilding) {
-      openButton(target, smallBuildingDescription);
-    }
-
-    if (stadium) {
-      openButton(target, stadiumDescription);
-    }
-
-    if (bridge) {
-      openButton(target, bridgeDescription);
-    }
-
-    if (flat) {
-      openButton(target, flatDescription);
-    }
-
-    if (museum) {
-      openButton(target, museumDescription);
-    }
-
-    if (showroom) {
-      openButton(target, showroomDescription);
-    }
-
-    if (administration) {
-      openButton(target, administrationDescription);
-    }
-
-    if (office1) {
-      openButton(target, office1Description);
-    }
-
-    if (office2) {
-      openButton(target, office2Description);
-    }
-
-    if (apartments) {
-      openButton(target, apartmentsDescription);
-    }
-
-    if (plusImg) {
-      if (target.parentNode.classList.contains('button_variant_small-building')) {
-        openButton(target.parentNode, smallBuildingDescription);
+    if (button != null) {
+      if (button.classList.contains('button_variant_small-building')) {
+        openButton(button, smallBuildingDescription);
       }
 
-      if (target.parentNode.classList.contains('button_variant_stadium')) {
-        openButton(target.parentNode, stadiumDescription);
+      if (button.classList.contains('button_variant_stadium')) {
+        openButton(button, stadiumDescription);
       }
 
-      if (target.parentNode.classList.contains('button_variant_bridge')) {
-        openButton(target.parentNode, bridgeDescription);
+      if (button.classList.contains('button_variant_bridge')) {
+        openButton(button, bridgeDescription);
       }
 
-      if (target.parentNode.classList.contains('button_variant_flat')) {
-        openButton(target.parentNode, flatDescription);
+      if (button.classList.contains('button_variant_flat')) {
+        openButton(button, flatDescription);
       }
 
-      if (target.parentNode.classList.contains('button_variant_museum')) {
-        openButton(target.parentNode, museumDescription);
+      if (button.classList.contains('button_variant_museum')) {
+        openButton(button, museumDescription);
       }
 
-      if (target.parentNode.classList.contains('button_variant_showroom')) {
-        openButton(target.parentNode, showroomDescription);
+      if (button.classList.contains('button_variant_showroom')) {
+        openButton(button, showroomDescription);
       }
 
-      if (target.parentNode.classList.contains('button_variant_administration')) {
-        openButton(target.parentNode, administrationDescription);
+      if (button.classList.contains('button_variant_administration')) {
+        openButton(button, administrationDescription);
       }
 
-      if (target.parentNode.classList.contains('button_variant_office1')) {
-        openButton(target.parentNode, office1Description);
+      if (button.classList.contains('button_variant_office1')) {
+        openButton(button, office1Description);
       }
 
-      if (target.parentNode.classList.contains('button_variant_office2')) {
-        openButton(target.parentNode, office2Description);
+      if (button.classList.contains('button_variant_office2')) {
+        openButton(button, office2Description);
       }
 
-      if (target.parentNode.classList.contains('button_variant_apartments')) {
-        openButton(target.parentNode, apartmentsDescription);
+      if (button.classList.contains('button_variant_apartments')) {
+        openButton(button, apartmentsDescription);
       }
     }
-
   } else {
     closeButton(openedButton);
     openedButton = null;
   }
 })
 
-function openButton(target, textOfSpan) {
-  const img = target.querySelector('img');
+function openButton(button, textOfSpan) {
+  const img = button.querySelector('img');
   img.src = '../src/images/minus.svg';
   img.alt = 'закрыть описание';
-  const span = target.querySelector('span');
+  const span = button.querySelector('span');
   span.textContent = textOfSpan;
   span.classList.add('button__description_open');
-  openedButton = target;
+  openedButton = button;
 }
 
 function closeButton(openedButton) {
